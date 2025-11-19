ef55404c401bc08a6ae49554283ddf56ee60a215
Paolo Mazzon <pdmazzon@gmail.com>
2025-11-19 12:29:32 -0800

    Flesh out buffer allocator class a bit

--- renderer/include/render/BufferAllocator.hpp
+++ renderer/include/render/BufferAllocator.hpp
@@ -1,13 +1,51 @@
 /// \brief C++ declaration of the temporary buffer abstraction's class
 #pragma once
+#include <vulkan/vulkan.h>
+#include <vector>
 
 namespace MVRender {
+    // MVR_Buffer is a pointer to one of these structs
+    struct BufferDescriptor {
+        VkBuffer buffer;
+        VkDeviceSize offset;
+        VkDeviceSize size;
+    };
+
+    // For internal use in BufferAllocator
+    struct BufferPage {
+        VkBuffer vram_buffer; // actual vulkan device memory
+        VkBuffer staging_buffer; // staging buffer that will be copied to vram
+        VkDeviceSize offset; // current offset for new writes
+        void *data; // data for the staging buffer
+    };
+
     // A paging temporary buffer allocator. There should be one of these
     // per frame in flight to allow users to allocate arbitrary temporary
     // buffers in vram.
     class BufferAllocator {
+        // List of temporary buffers
+        std::vector<BufferDescriptor> m_buffers;
+
+        // Pages of memory, both the staging and device memory
+        std::vector<BufferPage> m_buffer_pages;
+
+        // How many pages are actually in use
+        uint32_t m_page_count = 0;
+
+        // Size of each page
+        VkDeviceSize m_page_size = 0;
     public:
         BufferAllocator() = default;
-        // TODO: The rest of this
+        explicit BufferAllocator(VkDeviceSize page_size);
+        ~BufferAllocator();
+
+        // Returns a handle to a temporary buffer of size size and returns a pointer to its first byte of data
+        MVR_Buffer allocate_temp_buffer(VkDeviceSize size, void **data);
+
+        // Records necessary copy commands into the copy command buffer
+        void record_copy_commands(VkCommandBuffer command_buffer);
+
+        // Performs start of frame tasks.
+        void begin_frame();
     };
 }
--- renderer/src/Buffers.cpp
+++ renderer/src/Buffers.cpp
@@ -1,4 +1,25 @@
 #include "render/Buffers.h"
+#include "render/BufferAllocator.hpp"
+
+MVRender::BufferAllocator::BufferAllocator(VkDeviceSize page_size) {
+    m_page_size = page_size;
+}
+
+MVRender::BufferAllocator::~BufferAllocator() {
+    // TODO: This
+}
+
+MVR_Buffer MVRender::BufferAllocator::allocate_temp_buffer(VkDeviceSize size, void **data) {
+    // TODO: This
+}
+
+void MVRender::BufferAllocator::record_copy_commands(VkCommandBuffer command_buffer) {
+    // TODO: This
+}
+
+void MVRender::BufferAllocator::begin_frame() {
+    // TODO: This
+}
 
 MVR_API MVR_Result mvr_CreateTempBuffer(uint64_t size, void *data, MVR_Buffer *buffer) {
     // TODO: This
